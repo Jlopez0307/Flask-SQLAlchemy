d94f91d5d102945ccd73262379d796e059488a0f
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2023-01-03 22:06:13 -0500

    added new templates and routes

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db, Users
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+# 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Halo03117!@localhost:5432/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-
 connect_db(app)
-db.create_all()
+
+app.config['SECRET_KEY'] = 'cats'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+# db.create_all()
+
+@app.route('/')
+def home():
+    return redirect('/users')
+
+@app.route('/users')
+def list_users():
+    all_users = Users.query.all()
+
+    return render_template('base.html', users = all_users)
+
+@app.route('/users/new')
+def create_user():
+    return render_template('new_user.html')
+
+
